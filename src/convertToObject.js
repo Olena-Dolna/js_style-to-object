0cdc90cd0e36a6615943bd7a45a4c3a728d90ac0
Olena Dolna <dolnaolena2019@gmail.com>
2025-12-09 14:56:01 +0100

    solution added

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const commands = sourceString.split(';');
+  const commandsAndValues = commands.map((command) => command.split(':'));
+  const mergedCommandsAndValues = commandsAndValues.reduce(
+    (allValues, array) => [...allValues, ...array],
+    []
+  );
+  const trimmedCommandsAndValues = mergedCommandsAndValues.map((command) =>
+    command.trim()
+  );
+  const validCommandsAndValues = trimmedCommandsAndValues.filter(
+    (command) => command.length > 0
+  );
+  const result = {};
+
+  for (let i = 0; i < validCommandsAndValues.length - 1; i += 2) {
+    result[validCommandsAndValues[i]] = validCommandsAndValues[i + 1];
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
